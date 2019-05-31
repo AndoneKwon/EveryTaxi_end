b2f57efda910440201e6cb9910a4cc4cc7f5ffda
nova6296 <eppiko7@naver.com>
2019-05-31 18:10:12 +0900

    v0531_18:10

--- app/src/main/java/com/example/myapplication/SignInActivity.java
+++ app/src/main/java/com/example/myapplication/SignInActivity.java
@@ -3,19 +3,94 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class SignInActivity extends AppCompatActivity {
+    Button sign_in_btn;
     Button sign_up_btn;
 
+    private static final String TAG = "EveryTaxi";
+
+    String root_url = "https://everytaxi95.cafe24.com";
+    String sign_in_url = "https://everytaxi95.cafe24.com/sign_in_action.php";
+    String username;
+    String password;
+    String result;
+    TextView help_msg;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
         setTitle("로그인");
 
+        SSLConnect ssl = new SSLConnect();
+        ssl.postHttps(root_url, 1000, 1000);
+
         sign_up_btn = (Button) findViewById(R.id.sign_up_btn);
+        sign_in_btn = (Button) findViewById(R.id.sign_in_btn);
+        help_msg = (TextView) findViewById(R.id.sign_in_help_msg);
+
+        sign_in_btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                username = ((EditText) (findViewById(R.id.sign_in_username))).getText().toString();
+                password = ((EditText) (findViewById(R.id.sign_in_password))).getText().toString();
+
+                if (username.length() == 0 || password.length() == 0)
+                {
+                    Toast.makeText(SignInActivity.this, "아이디와 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    new Thread() {
+                        public void run()
+                        {
+                            result = HttpSignIn(sign_in_url, "", username, password);
+
+                            Log.d(TAG, "Login Result : " + result);
+
+                            if (result.equals("Success"))
+                            {
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                                startActivity(intent);
+                            }
+                            else if (result.equals("Connection Error"))
+                            {
+                                help_msg.setText("서버와의 연결에 문제가 발생했습니다.");
+                            }
+                            else if (result.equals("No Validation"))
+                            {
+                                help_msg.setText("아직 인증되지 않은 계정입니다. 잠시 후 시도해주세요.");
+                            }
+                            else if (result.equals("Wrong Input"))
+                            {
+                                help_msg.setText("아이디 또는 비밀번호 오류입니다.");
+                            }
+                            else
+                            {
+                                help_msg.setText("오류가 발생했습니다. 다시 시도해주세요.");
+                            }
+                        }
+                    }.start();
+                }
+            }
+        });
 
         sign_up_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +100,59 @@ public class SignInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public String HttpSignIn(String urlString, String params, String username, String password)
+    {
+        String send_msg = "";
+        String tmp_str = "";
+        String result = "";
+
+        try
+        {
+            URL connectUrl = new URL(urlString);
+
+            HttpURLConnection conn = (HttpURLConnection) connectUrl.openConnection();
+
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestMethod("POST");
+
+            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+
+            send_msg = "username=" + username + "&password=" + password;
+
+            osw.write(send_msg);
+            osw.flush();
+
+            if (conn.getResponseCode() == conn.HTTP_OK)
+            {
+                InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                BufferedReader reader = new BufferedReader(tmp);
+                StringBuffer buffer = new StringBuffer();
+
+                while ((tmp_str = reader.readLine()) != null)
+                {
+                    buffer.append(tmp_str);
+                }
+
+                result = buffer.toString();
+
+                Log.d(TAG, "Inner Result : " + result);
+            }
+            else
+            {
+                Log.i("통신 결과 : ", conn.getResponseCode() + " 에러");
+
+                result = "Connection Error";
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+
+            result = "Exception Error";
+        }
+
+        return result;
     }
 }
--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -16,10 +17,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -28,14 +41,15 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity
+{
     ImageView s_card_icon;
     ImageView p_picture_icon;
     Button select_s_card;
@@ -50,11 +64,14 @@ public class SignUpActivity extends AppCompatActivity {
     int select = 0;
 
     String root_url = "https://everytaxi95.cafe24.com";
-    String target = "https://everytaxi95.cafe24.com/sign_up_action.php";
+    String register_url = "https://everytaxi95.cafe24.com/register_action.php";
+    String file_upload_url = "https://everytaxi95.cafe24.com/file_upload_action.php";
+    String s_number;
     String username;
     String password;
     String confirm;
     String result;
+    TextView help_msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -71,6 +88,7 @@ public class SignUpActivity extends AppCompatActivity {
         select_s_card = (Button) findViewById(R.id.select_s_card);
         select_p_picture = (Button) findViewById(R.id.select_p_picture);
         sign_up_okay = (Button) findViewById(R.id.sign_up_okay);
+        help_msg = (TextView) findViewById(R.id.help_msg);
 
         select_s_card.setOnClickListener(new View.OnClickListener()
         {
@@ -96,11 +114,12 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
+                s_number = ((EditText) (findViewById(R.id.s_number))).getText().toString();
                 username = ((EditText) (findViewById(R.id.username))).getText().toString();
                 password = ((EditText) (findViewById(R.id.password))).getText().toString();
                 confirm = ((EditText) (findViewById(R.id.confirm))).getText().toString();
 
-                if (username.length() == 0 || password.length() == 0 || confirm.length() == 0)
+                if (s_number.length() == 0 || username.length() == 0 || password.length() == 0 || confirm.length() == 0)
                 {
                     Toast.makeText(SignUpActivity.this, "빈칸을 모두 작성해주세요.", Toast.LENGTH_SHORT).show();
                 }
@@ -111,34 +130,108 @@ public class SignUpActivity extends AppCompatActivity {
                 else if (s_card_tempFile == null || p_picture_tempFile == null)
                 {
                     Toast.makeText(SignUpActivity.this, "사진을 등록해주세요.", Toast.LENGTH_SHORT).show();
-
                 }
                 else
                 {
                     new Thread() {
                         public void run()
                         {
-                            DoFileUpload(target, s_card_tempFile.getAbsolutePath());
+                            result = HttpRegister(register_url, "", s_number, username, password);
 
-                            DoFileUpload(target, p_picture_tempFile.getAbsolutePath());
+                            Log.d(TAG, "Register Result : " + result);
+
+                            if (result.equals("Success"))
+                            {
+                                DoFileUpload_s_card(file_upload_url, s_card_tempFile.getAbsolutePath());
+
+                                DoFileUpload_p_picture(file_upload_url, p_picture_tempFile.getAbsolutePath());
+
+                                Intent intent = new Intent(getApplicationContext(), SignInActivity.class);
+
+                                startActivity(intent);
+                            }
+                            else if (result.equals("Connection Error"))
+                            {
+                                help_msg.setText("서버와의 연결에 문제가 발생했습니다.");
+                            }
+                            else
+                            {
+                                help_msg.setText("오류가 발생했습니다. 다시 시도해주세요.");
+                            }
                         }
                     }.start();
                 }
-
             }
         });
     }
 
-    public void DoFileUpload(String apiUrl, String absolutePath)
+    public String HttpRegister(String urlString, String params, String s_number, String username, String password)
     {
-        HttpFileUpload(apiUrl, "", absolutePath);
+        String send_msg = "";
+        String tmp_str = "";
+        String result = "";
+
+        try
+        {
+            URL connectUrl = new URL(urlString);
+
+            HttpURLConnection conn = (HttpURLConnection) connectUrl.openConnection();
+
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestMethod("POST");
+
+            OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+
+            send_msg = "s_number=" + s_number + "&username=" + username + "&password=" + password;
+
+            osw.write(send_msg);
+            osw.flush();
+
+            if (conn.getResponseCode() == conn.HTTP_OK)
+            {
+                InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                BufferedReader reader = new BufferedReader(tmp);
+                StringBuffer buffer = new StringBuffer();
+
+                while ((tmp_str = reader.readLine()) != null)
+                {
+                    buffer.append(tmp_str);
+                }
+
+                result = buffer.toString();
+            }
+            else
+            {
+                Log.i("통신 결과 : ", conn.getResponseCode() + " 에러");
+
+                result = "Connection Error";
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+
+            result = "Exception Error";
+        }
+
+        return result;
     }
 
-    public void HttpFileUpload(String urlString, String params, String fileName)
+    public void DoFileUpload_s_card(String apiUrl, String absolutePath)
+    {
+        HttpFileUpload_s_card(apiUrl, "", absolutePath);
+    }
+
+    public void HttpFileUpload_s_card(String urlString, String params, String fileName)
     {
         String lineEnd = "\r\n";
         String twoHyphens = "--";
         String boundary = "*****";
+        String file_save_name;
+
+        username = ((EditText) (findViewById(R.id.username))).getText().toString();
+
+        file_save_name = username + "_s_card";
 
         try
         {
@@ -168,7 +261,96 @@ public class SignUpActivity extends AppCompatActivity {
 
                 dos = new DataOutputStream(conn.getOutputStream());
                 dos.writeBytes(twoHyphens + boundary + lineEnd);
-                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\"" + fileName + "\"" + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\"" + file_save_name + "\"" + lineEnd);
+                dos.writeBytes(lineEnd);
+
+                int bytesAvailable = mFileInputStream.available();
+                int maxBufferSize = 1024 * 1024;
+                int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+
+                byte[] buffer = new byte[bufferSize];
+                int bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0)
+                {
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = mFileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+                }
+
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                mFileInputStream.close();
+                dos.flush();
+
+                if (conn.getResponseCode() == 200)
+                {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer stringBuffer = new StringBuffer();
+                    String line;
+
+                    line = reader.readLine();
+                }
+
+                mFileInputStream.close();
+
+                dos.close();
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void DoFileUpload_p_picture(String apiUrl, String absolutePath)
+    {
+        HttpFileUpload_p_picture(apiUrl, "", absolutePath);
+    }
+
+    public void HttpFileUpload_p_picture(String urlString, String params, String fileName)
+    {
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        String file_save_name;
+
+        username = ((EditText) (findViewById(R.id.username))).getText().toString();
+
+        file_save_name = username + "_p_picture";
+
+        try
+        {
+            File sourceFile = new File(fileName);
+            DataOutputStream dos;
+
+            if (!sourceFile.isFile())
+            {
+                Log.e("uploadFile", "Source File not exist :" + fileName);
+            }
+            else
+            {
+                FileInputStream mFileInputStream = new FileInputStream(sourceFile);
+
+                URL connectUrl = new URL(urlString);
+
+                HttpURLConnection conn = (HttpURLConnection) connectUrl.openConnection();
+
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", fileName);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\"" + file_save_name + "\"" + lineEnd);
                 dos.writeBytes(lineEnd);
 
                 int bytesAvailable = mFileInputStream.available();
