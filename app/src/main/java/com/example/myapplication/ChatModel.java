f0a892ab6f2a2eefa7f7a903f786777c37108b21
권진우 <wlsdn110@gmail.com>
2019-06-02 03:39:07 +0900

    board and chat

---
+++ app/src/main/java/com/example/myapplication/ChatModel.java
@@ -0,0 +1,9 @@
+package com.example.myapplication;
+
+public class ChatModel {
+        public static class Comment {
+
+            public String uid;
+            public String message;
+        }
+}
--- app/src/main/java/com/example/myapplication/MessageActivity.java
+++ app/src/main/java/com/example/myapplication/MessageActivity.java
@@ -1,21 +1,30 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 
@@ -25,10 +34,12 @@ public class MessageActivity extends AppCompatActivity {
     EditText editText;
     Button sendButton;
 
+    private RecyclerView recyclerView;
 
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("message");
+    mLayoutManager = new LinearLayoutManager(this);
 
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
@@ -39,16 +50,18 @@ public class MessageActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, // 한 줄에 하나의 텍스트 아이템만 보여주는 레이아웃 파일
                 android.R.id.text1  // 데이터가 저장되어 있는 ArrayList 객체
         );
-        listView = (ListView) findViewById(R.id.listView);
+        //listView = (ListView) findViewById(R.id.listView);
         editText = (EditText) findViewById(R.id.editText);
         sendButton = (Button) findViewById(R.id.button);
+
+
 
         SharedPreferences sharedPreferences = getSharedPreferences("cookie",MODE_PRIVATE);
         username = sharedPreferences.getString("username", "");
 
 // 기본 Text를 담을 수 있는 simple_list_item_1을 사용해서 ArrayAdapter를 만들고 listview에 설정
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
-        listView.setAdapter(adapter);
+//        listView.setAdapter(adapter);
 
         sendButton.setOnClickListener(new View.OnClickListener()
         {
@@ -62,12 +75,17 @@ public class MessageActivity extends AppCompatActivity {
             }
         });
 
+
+
         final ArrayAdapter<String> finalAdapter = adapter;
         myRef.child("message").addChildEventListener(new ChildEventListener() {  // message는 child의 이벤트를 수신합니다.
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 ChatData chatData = dataSnapshot.getValue(ChatData.class);  // chatData를 가져오고
-                finalAdapter.add(chatData.getUserName() + ": " + chatData.getMessage());  // adapter에 추가합니다.
+                //finalAdapter.add(chatData.getUserName() + ": " + chatData.getMessage());  // adapter에 추가합니다.
+                sendButton.setEnabled(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(MessageActivity.this));
+                recyclerView.setAdapter(new RecyclerViewAdapter());
             }
 
             @Override
@@ -82,6 +100,7 @@ public class MessageActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) { }
         });
+
     }
     public void setMyRef(DatabaseReference myRef) {
         this.myRef = myRef;
@@ -89,5 +108,65 @@ public class MessageActivity extends AppCompatActivity {
 
     public DatabaseReference getMyRef() {
         return myRef;
+    }
+
+    class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+
+        List<ChatModel.Comment> comments;
+        public RecyclerViewAdapter() {
+            comments = new ArrayList<>();
+
+            FirebaseDatabase.getInstance().getReference().child("chat").child("comments").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    comments.clear();
+
+                    for(DataSnapshot item : dataSnapshot.getChildren()){
+                        comments.add(item.getValue(ChatModel.Comment.class));
+                    }
+
+                    notifyDataSetChanged();
+
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleview_item,parent,false);
+
+
+            return new MessageViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+
+            ((MessageViewHolder)holder).textView_message.setText(comments.get(position).message);
+        }
+
+        @Override
+        public int getItemCount() {
+            return comments.size();
+        }
+
+        private class MessageViewHolder extends RecyclerView.ViewHolder {
+            public TextView textView_message;
+
+            public MessageViewHolder(View view) {
+                super(view);
+                textView_message = (TextView) view.findViewById(R.id.textView_message);
+            }
+        }
     }
 }
